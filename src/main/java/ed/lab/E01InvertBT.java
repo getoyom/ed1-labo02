4ef76a139c0833e98527792cc6689e02ef2a9d7e
getoyom <getoyom@correo.url.edu.gt>
2025-03-18 08:23:00 -0600

    Problema 01 resuelto

--- src/main/java/ed/lab/E01InvertBT.java
+++ src/main/java/ed/lab/E01InvertBT.java
@@ -2,6 +2,11 @@ package ed.lab;
 
 public class E01InvertBT {
     public TreeNode<Integer> invertTree(TreeNode<Integer> root) {
+        if (root == null) {return null;} //Return null
+        TreeNode left = invertTree(root.left); //Transverse left node
+        TreeNode right = invertTree(root.right); //Transverse right node
+        root.right = left; //Switch right node with left node
+        root.left = right; //Switch left node with right node
         return root;
     }
 }
--- src/main/java/ed/lab/E02KthSmallest.java
+++ src/main/java/ed/lab/E02KthSmallest.java
@@ -1,7 +1,7 @@
 package ed.lab;
 
 public class E02KthSmallest {
-
+    final int minValue = 0;
     public int kthSmallest(TreeNode<Integer> root, int k) {
         return 0;
     }
